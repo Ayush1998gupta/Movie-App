3144d063b5f7c7bcfc4641fa634f926544feb8ca
Ayush <aayush199813@gmail.com>
2022-07-12 19:08:12 +0530

    added redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 
+const store = createStore(movies);
+console.log('store',store);
+console.log('STORE',store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
