1e9f64e3db138e852a91a34745009b84456df6c5
Ayush <aayush199813@gmail.com>
2022-07-12 21:16:57 +0530

    Action Creators

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+
+// action types
+export const ADD_MOVIES = 'ADD_MOVIES';
+
+
+// action creators
+export function addMovies(movies) {
+   return {
+      type: ADD_MOVIES,
+      movies:movies
+   }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { data } from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import React from 'react';
+import {addMovies} from '../actions'
 
 class App extends React.Component {
 
@@ -15,10 +16,7 @@ class App extends React.Component {
     });
       // make api call
     // dispatch action
-    store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
+    store.dispatch(addMovies(data));
 
     console.log('STATE ', this.props.store.getState());
   }
